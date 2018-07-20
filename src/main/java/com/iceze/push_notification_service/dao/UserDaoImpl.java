ce679efd258c92a97416772aeb1f500a4d5ce134
Miroslav <miroslav25a@yahoo.co.uk>
2018-07-20 16:52:34 +0100

    fix tests

--- src/main/java/com/iceze/push_notification_service/dao/UserDaoImpl.java
+++ src/main/java/com/iceze/push_notification_service/dao/UserDaoImpl.java
@@ -34,5 +34,4 @@ public class UserDaoImpl implements UserDao {
 		
 		return Optional.of(user);
 	}
-
 }
--- src/test/java/com/iceze/push_notification_service/service/BasicUserServiceTest.java
+++ src/test/java/com/iceze/push_notification_service/service/BasicUserServiceTest.java
@@ -2,21 +2,34 @@ package com.iceze.push_notification_service.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.util.Optional;
 import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import com.google.common.collect.Sets;
+import com.iceze.push_notification_service.dao.UserDao;
 import com.iceze.push_notification_service.model.User;
 
 public class BasicUserServiceTest {
+	@InjectMocks
 	private BasicUserService service;
+	@Mock
+	private UserDao userDao;
 	
 	@Before
 	public void setup() {
-		this.service = new BasicUserService();
+		initMocks(this);
 	}
 
 	@Test
@@ -25,19 +38,25 @@ public class BasicUserServiceTest {
 				   				   .withUsername("user1")
 				   				   .build();
 		
-		service.saveUser(user1);
+		when(this.userDao.findAllUsers()).thenReturn(Optional.of(Sets.newHashSet(user1)));
 		
 		Optional<Set<User>> result = service.findAllUsers();
 		
 		assertThat(result).isNotNull();
 		assertThat(result.get()).isNotEmpty().contains(user1);
+		
+		verify(this.userDao).findAllUsers();
 	}
 	
 	@Test
 	public void saveUserShould() {
-		Optional<User> result = this.service.saveUser(User.builder().withAccessToken("token")
-																	.withUsername("user1")
-																	.build());
+		User user = User.builder().withAccessToken("token")
+								  .withUsername("user1")
+								  .build();
+		when(this.userDao.findAllUsers()).thenReturn(Optional.of(Sets.newHashSet()));
+		when(this.userDao.saveUser(any(User.class))).thenReturn(Optional.of(user));
+		
+		Optional<User> result = this.service.saveUser(user);
 		
 		assertThat(result).isNotNull();
 		assertThat(result.isPresent()).isTrue();
@@ -45,6 +64,9 @@ public class BasicUserServiceTest {
 		assertThat(result.get().getUsername()).isNotNull().isEqualTo("user1");
 		assertThat(result.get().getCreationTime()).isNotBlank();
 		assertThat(result.get().getNumOfNotificationsPushed()).isNotNull().isEqualTo(0);
+		
+		verify(this.userDao).findAllUsers();
+		verify(this.userDao).saveUser(any(User.class));
 	}
 	
 	@Test
@@ -52,14 +74,16 @@ public class BasicUserServiceTest {
 		User user = User.builder().withAccessToken("token")
 								  .withUsername("user1")
 								  .build();
-		
-		this.service.saveUser(user);
+		when(this.userDao.findAllUsers()).thenReturn(Optional.of(Sets.newHashSet(user)));
 		
 		assertThatExceptionOfType(ExistingUserException.class)
 		    	.isThrownBy(() -> this.service.saveUser(user))
 		    	.withMessage("User with username user1 already exists")
 		    	.withStackTraceContaining("ExistingUserException")
 		    	.withNoCause();
+		
+		verify(this.userDao).findAllUsers();
+		verify(this.userDao, times(0)).saveUser(any(User.class));
 	}
 	
 	@Test
@@ -68,13 +92,15 @@ public class BasicUserServiceTest {
 				  .withUsername("user1")
 				  .build();
 
-		this.service.saveUser(user);
+		when(this.userDao.findAllUsers()).thenReturn(Optional.of(Sets.newHashSet(user)));
 		
 		Optional<User> result = this.service.findByUsername("user1");
 		
 		assertThat(result).isNotNull();
 		assertThat(result.isPresent()).isTrue();
 		assertThat(result.get().getUsername()).isNotNull().isEqualTo("user1");
+		
+		verify(this.userDao).findAllUsers();
 	}
 	
 	@Test
@@ -83,12 +109,14 @@ public class BasicUserServiceTest {
 				  .withUsername("user1")
 				  .build();
 
-		this.service.saveUser(user);
+		when(this.userDao.findAllUsers()).thenReturn(Optional.of(Sets.newHashSet(user)));
 		
 		Optional<User> result = this.service.findByUsername("user2");
 		
 		assertThat(result).isNotNull();
 		assertThat(result.isPresent()).isFalse();
+		
+		verify(this.userDao).findAllUsers();
 	}
 	
 	@Test
@@ -97,7 +125,7 @@ public class BasicUserServiceTest {
 				  				  .withUsername("user1")
 				  				  .build();
 		
-		this.service.saveUser(user);
+		when(this.userDao.findAllUsers()).thenReturn(Optional.of(Sets.newHashSet(user)));
 		
 		User updateUser = User.builder().withAccessToken("update token")
 				  						.withUsername("user1")
@@ -113,6 +141,9 @@ public class BasicUserServiceTest {
 		assertThat(result.get().getAccessToken()).isNotNull().isEqualTo("update token");
 		assertThat(result.get().getCreationTime()).isNotNull().isEqualTo("update time");
 		assertThat(result.get().getNumOfNotificationsPushed()).isNotNull().isEqualTo(1);
+		
+		verify(this.userDao, times(2)).findAllUsers();
+		verify(this.userDao).saveUser(any(User.class));
 	}
 	
 	@Test
@@ -121,7 +152,7 @@ public class BasicUserServiceTest {
 								  .withUsername("user1")
 								  .build();
 		
-		this.service.saveUser(user);
+		when(this.userDao.findAllUsers()).thenReturn(Optional.of(Sets.newHashSet(user)));
 		
 		User updateUser = User.builder().withAccessToken("update token")
 										.withUsername("user2")
@@ -134,6 +165,9 @@ public class BasicUserServiceTest {
 		    	.withMessage("User not found")
 		    	.withStackTraceContaining("InvalidUserException")
 		    	.withNoCause();
+		
+		verify(this.userDao).findAllUsers();
+		verify(this.userDao, times(0)).saveUser(any(User.class));
 	}
 	
 	@Test
@@ -217,12 +251,14 @@ public class BasicUserServiceTest {
 								  .withUsername("user1")
 								  .build();
 		
-		this.service.saveUser(user);
+		when(this.userDao.findAllUsers()).thenReturn(Optional.of(Sets.newHashSet(user)));
 		
 		assertThatExceptionOfType(ExistingUserException.class)
 		    	.isThrownBy(() -> this.service.validate(user))
 		    	.withMessage("User with username user1 already exists")
 		    	.withStackTraceContaining("ExistingUserException")
 		    	.withNoCause();
+		
+		verify(this.userDao).findAllUsers();
 	}
 }
